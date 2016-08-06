55b41cb3b85a0e05dff6b61fc9d1100c996f9740
petew <peter.worthy@uqconnect.edu.au>
2016-08-06 20:16:50 +1000

    Brisbane Page completed
    Completed brisbane page - v1.  Main functionality for pages completed.
    Need to still create Mt Isa and Gladstone.

--- php/get/getBneLandWaste.php
+++ php/get/getBneLandWaste.php
@@ -4,14 +4,14 @@
 
 require_once '../inc/db_connect.php';
 
-$stmt = $db->prepare("SELECT SUM(quantity_in_kg) FROM wastedata WHERE Type='Land' AND site_address_suburb='Murarrie'");
+$stmt = $db->prepare("SELECT * FROM wastedata WHERE Type='Land' AND site_address_suburb='Murarrie'");
 $stmt->execute();
-//$result = $stmt->fetchAll();
-$result = $stmt->fetch();
-//$result = json_encode($result);
+$result = $stmt->fetchAll();
+//$result = $stmt->fetch();
+$result = json_encode($result);
 
-echo $result[0];
-
+//echo $result[0];
+echo $result;
 
 
 ?>
