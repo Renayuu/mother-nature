de3643cc84bb890f3f889ec7076a19a7fcb6cfb0
petew <peter.worthy@uqconnect.edu.au>
2016-08-06 13:04:43 +1000

    Update test index
    Pulling data from db - just air

--- js/mn_1.js
+++ js/mn_1.js
@@ -3,7 +3,30 @@ $(document).ready(function(){
 	console.log ("hello from mother nature");
 	
 	$.get("php/get/getBneAirWaste.php", function(data, status){
-		console.log(data[0].substance);
+		let airData = $.parseJSON(data);
+		let airCode = "";
+		
+		airCode += "<table class='table table-condensed'>";
+		
+		for (let i=0; i<airData.length; i++)
+		{
+			
+			console.log(airData[i].substance + " " + airData[i].quantity_in_kg);
+			airCode += "<tr>";
+			airCode += "<td>";
+			airCode += airData[i].substance;
+			airCode += "</td>";
+			airCode += "<td>";
+			airCode += airData[i].quantity_in_kg
+			airCode += "</td>";
+			airCode += "</td>";
+			
+		}
+		
+		airCode += "</table>";
+		
+		$("#airdata").html(airCode);
+		
 	})
 	
 	
